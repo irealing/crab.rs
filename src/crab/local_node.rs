d3af33f1137c91ad807b1b354a0792df0fd3b108
Memory_Leak <irealing@163.com>
2026-05-18 16:11:34 +0800

    Refactor LocalNodeInner to LocalNode with async accept and listen
    Introduce `LocalNode` as a public wrapper around `LocalNodeInner`, move
    the
    handshake loop logic into an `

--- src/crab/local_node.rs
+++ src/crab/local_node.rs
@@ -5,6 +5,7 @@ use async_trait::async_trait;
 use quinn::crypto::rustls::QuicServerConfig;
 use quinn::{Endpoint, ServerConfig};
 use tokio::select;
+use tokio::sync::mpsc::{self, Receiver};
 use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
 
@@ -23,59 +24,29 @@ pub struct Config {
     pub listen: String,
 }
 pub trait LocalEndpoint: Node + Worker {}
-pub struct LocalNodeInner {
+struct LocalNodeInner {
     node_id: String,
     endpoint: Arc<Endpoint>,
     manager: Arc<dyn Manager>,
 }
-impl LocalNodeInner {
-    async fn handshake(&self, _: quinn::Connection) -> Result<Arc<dyn Node>, CrabError> {
-        todo!("handshake");
-    }
+pub struct LocalNode {
+    inner: Arc<LocalNodeInner>,
 }
-impl LocalNodeInner {
-    async fn new(cfg: Config, tls_cfg: TLSConfig) -> Result<Self, CrabError> {
-        let addr = cfg.listen.parse::<SocketAddr>().map_err(|e| {
-            log::warn!("bad addr {} error:{}", &cfg.listen, e);
-            CrabError::ErrorCode(CrabError::BAD_ADDR)
-        })?;
-        let mut server_crypto_cfg = TLSProvider::from_config(tls_cfg).build_server_config()?;
-        server_crypto_cfg.alpn_protocols = vec![CRAB_TLS_ALPN.to_vec()];
-        let quic_cfg = ServerConfig::with_crypto(Arc::new(
-            QuicServerConfig::try_from(server_crypto_cfg)
-                .map_err(|_| CrabError::ErrorCode(CrabError::CRYPTO_ERROR))?,
-        ));
-        let endpoint = Endpoint::server(quic_cfg, addr).map_err(|e| {
-            log::warn!("bind quic addr {} error {}", cfg.listen, e);
-            e
-        })?;
-        Ok(LocalNodeInner {
-            node_id: cfg.id,
-            endpoint: Arc::new(endpoint),
-            manager: Arc::new(default_node_manager()),
-        })
-    }
-}
-impl LocalEndpoint for LocalNodeInner {}
-#[async_trait]
-impl Node for LocalNodeInner {
-    fn id(&self) -> &str {
-        return &self.node_id;
-    }
-}
-#[async_trait]
-impl Worker for LocalNodeInner {
-    async fn run(&self, cancel: CancellationToken) -> Option<CrabError> {
-        log::info!("local node {} start", &self.node_id);
+impl LocalNode {
+    async fn accept(
+        local_node: Arc<LocalNodeInner>,
+        cancel: CancellationToken,
+        tx: mpsc::Sender<Arc<dyn Node>>,
+    ) -> Result<(), CrabError> {
         let mut tasks = JoinSet::new();
         loop {
             select! {
                 _=cancel.cancelled()=>{
                     break;
                 },
-                accepted=self.endpoint.accept()=>{
+                accepted=local_node.endpoint.accept()=>{
                     if let Some(incoming)=accepted{
-                        let manager=Arc::clone(&self.manager);
+                        let manager=Arc::clone(&local_node.manager);
                         tasks.spawn(async move {
                             let conn=incoming.await.map_err(|e|{
                                 log::warn!("quic connection handshake error {}",e);
@@ -93,6 +64,7 @@ impl Worker for LocalNodeInner {
                         Ok(ret)=>{
                             if let Ok(node)=ret{
                                 log::info!("node {} handshake success",node.id());
+                               let _= tx.send(node).await.inspect_err(|e|{log::warn!("send handshaked node ({}) failed",e)});
                             }
                         },
                         Err(e)=>log::warn!("quic connection handshake error {}",e),
@@ -101,6 +73,66 @@ impl Worker for LocalNodeInner {
             }
         }
         tasks.shutdown().await;
+        Ok(())
+    }
+    async fn start_origin_node(mut rx: Receiver<Arc<dyn Node>>) -> Result<(), CrabError> {
+        Ok(())
+    }
+    async fn listen(&self, cancel: CancellationToken) -> Result<(), CrabError> {
+        log::warn!("start listn");
+        let (tx, mut rx) = mpsc::channel(32);
+        let mut join_set = JoinSet::new();
+        let accept_cancel = cancel.clone();
+        let local_node = self.inner.clone();
+        join_set.spawn(async move { Self::accept(local_node, accept_cancel, tx).await });
+        join_set.spawn(async move { Self::start_origin_node(rx).await });
+        while let Some(ret) = join_set.join_next().await {
+            match ret {
+                Ok(_) => continue,
+                Err(e) => {
+                    log::warn!("local node listen join error {}", e)
+                }
+            }
+        }
+        Ok(())
+    }
+}
+impl LocalNode {
+    async fn new(cfg: Config, tls_cfg: TLSConfig) -> Result<Self, CrabError> {
+        let addr = cfg.listen.parse::<SocketAddr>().map_err(|e| {
+            log::warn!("bad addr {} error:{}", &cfg.listen, e);
+            CrabError::ErrorCode(CrabError::BAD_ADDR)
+        })?;
+        let mut server_crypto_cfg = TLSProvider::from_config(tls_cfg).build_server_config()?;
+        server_crypto_cfg.alpn_protocols = vec![CRAB_TLS_ALPN.to_vec()];
+        let quic_cfg = ServerConfig::with_crypto(Arc::new(
+            QuicServerConfig::try_from(server_crypto_cfg)
+                .map_err(|_| CrabError::ErrorCode(CrabError::CRYPTO_ERROR))?,
+        ));
+        let endpoint = Endpoint::server(quic_cfg, addr).map_err(|e| {
+            log::warn!("bind quic addr {} error {}", cfg.listen, e);
+            e
+        })?;
+        Ok(LocalNode {
+            inner: Arc::new(LocalNodeInner {
+                node_id: cfg.id,
+                endpoint: Arc::new(endpoint),
+                manager: Arc::new(default_node_manager()),
+            }),
+        })
+    }
+}
+impl LocalEndpoint for LocalNode {}
+#[async_trait]
+impl Node for LocalNode {
+    fn id(&self) -> &str {
+        return &self.inner.node_id;
+    }
+}
+#[async_trait]
+impl Worker for LocalNode {
+    async fn run(&self, cancel: CancellationToken) -> Option<CrabError> {
+        log::warn!("local node start");
         None
     }
 }
@@ -108,19 +140,19 @@ pub async fn create_local_node(
     c: Config,
     tls_cfg: TLSConfig,
 ) -> Result<impl LocalEndpoint, CrabError> {
-    LocalNodeInner::new(c, tls_cfg).await
+    LocalNode::new(c, tls_cfg).await
 }
 mod tests {
     use std::sync::Arc;
 
     use crate::crab::default_node_manager;
 
-    use super::{Config, LocalNodeInner, TLSConfig};
+    use super::{Config, LocalNode, TLSConfig};
 
     #[tokio::test]
     async fn test_create_local_node() {
         let tls_cfg = TLSConfig::load_default_config_file();
-        LocalNodeInner::new(
+        LocalNode::new(
             Config {
                 id: "12345".to_string(),
                 listen: "127.0.0.1:65522".to_string(),
