b60049b350d0e34346a0df36f9c5e60da6660c93
Memory_Leak <irealing@163.com>
2026-08-14 14:26:29 +0800

    fixed: 尝试修正tcp_forward

--- src/app/protocol/forwarder/tcp_util.rs
+++ src/app/protocol/forwarder/tcp_util.rs
@@ -1,8 +1,8 @@
 use crab::CrabError;
 use crab::proto::Stream;
+use tokio::io::copy;
 use tokio::net::TcpStream;
 use tokio_util::sync::CancellationToken;
-
 pub async fn tcp_forward(
     cancel: CancellationToken,
     stream: Stream,
@@ -10,28 +10,21 @@ pub async fn tcp_forward(
 ) -> Result<(), CrabError> {
     let (mut quic_writer, mut quic_reader) = stream.split();
     let (mut tcp_reader, mut tcp_writer) = conn.split();
+    let forward_fut = async move {
+        let s_to_t = copy(&mut quic_reader, &mut tcp_writer);
+        let t_to_s = copy(&mut tcp_reader, &mut quic_writer);
+        tokio::join!(s_to_t, t_to_s)
+    };
     tokio::select! {
         _=cancel.cancelled()=>{
             log::error!("TCP Forwarder task cancelled.");
         }
-        write_ret=tokio::io::copy(&mut quic_reader,&mut tcp_writer) => {
-            match write_ret {
-                Err(e) => {
-                    log::error!("TCP Forwarder task write error. {}", e);
-                }
-                Ok(size)=>{
-                    log::trace!("TCP Forwarder task write request size: {}", size);
-                }
+        (write_ret, read_ret) = forward_fut => {
+            if let Err(e) = write_ret {
+                log::error!("QUIC -> TCP write error: {}", e);
             }
-        }
-        read_ret=tokio::io::copy(&mut tcp_reader, &mut quic_writer) => {
-            match read_ret {
-                Err(e) => {
-                    log::error!("TCP Forwarder task read error. {}", e);
-                }
-                Ok(size)=>{
-                    log::trace!("TCP Forwarder task read request size: {}", size);
-                }
+            if let Err(e) = read_ret {
+                log::error!("TCP -> QUIC read error: {}", e);
             }
         }
     }
